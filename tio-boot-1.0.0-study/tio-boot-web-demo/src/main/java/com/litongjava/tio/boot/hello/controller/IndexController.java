12abbc27afa0a9bccb5894d28f051dc1ae6c45f5
litongjava <litongjava@qq.com>
2023-10-16 02:24:48 -1000

    整合热加载失败

--- tio-boot-1.0.0-study/tio-boot-web-demo/src/main/java/com/litongjava/tio/boot/hello/controller/IndexController.java
+++ tio-boot-1.0.0-study/tio-boot-web-demo/src/main/java/com/litongjava/tio/boot/hello/controller/IndexController.java
@@ -5,7 +5,9 @@ import org.tio.http.common.HttpResponse;
 import org.tio.http.server.annotation.RequestPath;
 import org.tio.http.server.util.Resps;
 
+import com.litongjava.jfinal.aop.Aop;
 import com.litongjava.jfinal.aop.Autowired;
+import com.litongjava.tio.boot.context.TioApplicationContext;
 import com.litongjava.tio.boot.hello.services.IndexService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -16,19 +18,33 @@ import lombok.extern.slf4j.Slf4j;
 public class IndexController {
 
 //  private IndexService indexService = Aop.get(IndexService.class);
-  //不支持在Controller中使用@Autowired注解
+  // 不支持在Controller中使用@Autowired注解
   @Autowired
   private IndexService indexService;
 
   @RequestPath
   public HttpResponse respText(HttpRequest request) throws Exception {
     log.info("txt");
-    HttpResponse ret = Resps.txt(request, "hello");
+    HttpResponse ret = Resps.txt(request, "hello 3");
     return ret;
   }
 
   @RequestPath("get")
   public HttpResponse json(HttpRequest request) {
     return Resps.json(request, indexService.getUser());
+  }
+
+  @RequestPath("isRunning")
+  public HttpResponse isRunning(HttpRequest request) {
+    TioApplicationContext tioApplicationContext = Aop.get(TioApplicationContext.class);
+    boolean running = tioApplicationContext.isRunning();
+    return Resps.json(request, running);
+  }
+
+  @RequestPath("close")
+  public HttpResponse close(HttpRequest request) {
+    TioApplicationContext tioApplicationContext = Aop.get(TioApplicationContext.class);
+    tioApplicationContext.close();
+    return Resps.json(request, "OK");
   }
 }
