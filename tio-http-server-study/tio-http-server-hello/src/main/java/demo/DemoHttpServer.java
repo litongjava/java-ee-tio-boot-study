4307f1aaa6f4c03d1c6fbca5b5fb2d0e8a0d1c39
litongjava <litongjava@qq.com>
2023-12-26 00:33:30 -1000

    chage profile to server-graalvm

--- tio-http-server-study/tio-http-server-hello/src/main/java/demo/DemoHttpServer.java
+++ tio-http-server-study/tio-http-server-hello/src/main/java/demo/DemoHttpServer.java
@@ -3,49 +3,37 @@ package demo;
 import java.io.IOException;
 
 import com.litongjava.tio.http.common.HttpConfig;
-import com.litongjava.tio.http.common.HttpRequest;
-import com.litongjava.tio.http.common.HttpResponse;
 import com.litongjava.tio.http.common.handler.HttpRequestHandler;
 import com.litongjava.tio.http.server.HttpServerStarter;
 import com.litongjava.tio.http.server.handler.HttpRoutes;
 import com.litongjava.tio.http.server.handler.SimpleHttpDispatcherHandler;
 import com.litongjava.tio.http.server.handler.SimpleHttpRoutes;
-import com.litongjava.tio.http.server.util.Resps;
+
+import demo.controller.IndexController;
 
 public class DemoHttpServer {
 
   public static void main(String[] args) throws IOException {
-    // 手动添加路由
-    DemoHttpServer controller = new DemoHttpServer();
 
+    // 实例化Controller
+    IndexController controller = new IndexController();
+
+    // 手动添加路由
     HttpRoutes simpleHttpRoutes = new SimpleHttpRoutes();
     simpleHttpRoutes.add("/", controller::index);
     simpleHttpRoutes.add("/login", controller::login);
     simpleHttpRoutes.add("/exception", controller::exception);
-    //
+
+    // 配置服务服务器
     HttpConfig httpConfig;
     HttpRequestHandler requestHandler;
     HttpServerStarter httpServerStarter;
 
-    // httpConfig
     httpConfig = new HttpConfig(80, null, null, null);
     requestHandler = new SimpleHttpDispatcherHandler(httpConfig, simpleHttpRoutes);
     httpServerStarter = new HttpServerStarter(httpConfig, requestHandler);
+    // 启动服务器
     httpServerStarter.start();
-  }
-
-  public HttpResponse index(HttpRequest request) {
-    return Resps.txt(request, "index");
-
-  }
-
-  private HttpResponse login(HttpRequest request) {
-    return Resps.txt(request, "login");
-  }
-
-  private HttpResponse exception(HttpRequest request) {
-    throw new RuntimeException("error");
-    // return Resps.txt(request, "exception");
   }
 
 }
