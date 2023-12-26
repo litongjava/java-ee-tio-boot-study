0889d51f32afd644abdfd1f196adc0ec4282612d
litongjava <litongjava@qq.com>
2023-12-26 00:09:18 -1000

    add tio-http-server-hello

--- tio-boot-1.0.8-study/tio-boot-1.0.8-web-hello/src/main/java/com/litongjava/tio/web/hello/App.java
+++ tio-boot-1.0.8-study/tio-boot-1.0.8-web-hello/src/main/java/com/litongjava/tio/web/hello/App.java
@@ -1,13 +1,19 @@
 package com.litongjava.tio.web.hello;
 
-import com.litongjava.hotswap.wrapper.tio.boot.TioApplicationWrapper;
 import org.tio.utils.jfinal.P;
 
+import com.litongjava.hotswap.debug.Diagnostic;
+import com.litongjava.hotswap.wrapper.tio.boot.TioApplicationWrapper;
+import com.litongjava.jfinal.aop.annotation.ComponentScan;
+
+@ComponentScan
 public class App {
   public static void main(String[] args) {
     long start = System.currentTimeMillis();
     P.use("app.properties");
+    Diagnostic.setDebug(true);
     TioApplicationWrapper.run(App.class, args);
+//    TioApplication.run(App.class, args);
     long end = System.currentTimeMillis();
     System.out.println((end - start) + "ms");
   }
