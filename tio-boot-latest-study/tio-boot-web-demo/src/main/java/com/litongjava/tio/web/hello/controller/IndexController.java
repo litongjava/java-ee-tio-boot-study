d70e15144ac8790749db184d8f7d6d99b8c7d168
litongjava <litongjava@qq.com>
2024-03-30 16:56:49 -1000

    add json exmaple

--- tio-boot-latest-study/tio-boot-web-demo/src/main/java/com/litongjava/tio/web/hello/controller/IndexController.java
+++ tio-boot-latest-study/tio-boot-web-demo/src/main/java/com/litongjava/tio/web/hello/controller/IndexController.java
@@ -1,26 +1,41 @@
 package com.litongjava.tio.web.hello.controller;
 
-import com.litongjava.jfinal.aop.annotation.AComponentScan;
-import com.litongjava.jfinal.aop.annotation.AController;
 import com.litongjava.tio.http.common.HttpRequest;
 import com.litongjava.tio.http.common.HttpResponse;
 import com.litongjava.tio.http.server.annotation.RequestPath;
 import com.litongjava.tio.http.server.util.Resps;
+import com.litongjava.tio.utils.resp.RespVo;
 
-@AComponentScan
-@AController
 @RequestPath("/")
 public class IndexController {
   @RequestPath()
   public String index() {
     return "index";
   }
-  
+
   @RequestPath("customResponseCode")
-  public HttpResponse customResponseCode(HttpRequest request,int code) {
+  public HttpResponse customResponseCode(HttpRequest request, int code) {
     HttpResponse httpResponse = Resps.txt(request, "custom response code");
     httpResponse.setStatus(code);
     return httpResponse;
   }
-}
 
+  @RequestPath("json")
+  public HttpResponse json(HttpRequest request) {
+    RespVo fail = RespVo.fail("json");
+    HttpResponse response = new HttpResponse(request);
+    return response.setJson(fail);
+  }
+
+  @RequestPath("json2")
+  public HttpResponse json2(HttpRequest request) {
+    RespVo fail = RespVo.fail("json");
+    return HttpResponse.json(request, fail);
+  }
+
+  @RequestPath("json3")
+  public HttpResponse json3() {
+    RespVo fail = RespVo.fail("json");
+    return HttpResponse.json(fail);
+  }
+}
