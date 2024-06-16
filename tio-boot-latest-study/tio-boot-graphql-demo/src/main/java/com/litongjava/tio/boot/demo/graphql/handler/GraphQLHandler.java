b56f90c30be5c8a845f37dbc33f9dcad77ccc8e2
litongjava <litongjava@qq.com>
2024-06-16 06:14:48 -1000

    update imports

--- tio-boot-latest-study/tio-boot-graphql-demo/src/main/java/com/litongjava/tio/boot/demo/graphql/handler/GraphQLHandler.java
+++ tio-boot-latest-study/tio-boot-graphql-demo/src/main/java/com/litongjava/tio/boot/demo/graphql/handler/GraphQLHandler.java
@@ -2,9 +2,9 @@ package com.litongjava.tio.boot.demo.graphql.handler;
 
 import java.util.Map;
 
-import com.litongjava.data.utils.TioRequestParamUtils;
 import com.litongjava.jfinal.plugin.graphql.GQL;
 import com.litongjava.tio.boot.http.TioControllerContext;
+import com.litongjava.tio.boot.utils.TioRequestParamUtils;
 import com.litongjava.tio.http.common.HttpRequest;
 import com.litongjava.tio.http.common.HttpResponse;
 import com.litongjava.tio.utils.resp.Resp;
@@ -16,12 +16,12 @@ public class GraphQLHandler {
 
   public HttpResponse graphql(HttpRequest httpRequest) {
     Map<String, Object> requestMap = TioRequestParamUtils.getRequestMap(httpRequest);
-
+    
     String query = (String) requestMap.get("query");
 
     @SuppressWarnings("unchecked")
     Map<String, Object> variables = (Map<String, Object>) requestMap.get("variables");
-
+    
     ExecutionInput executionInput = ExecutionInput.newExecutionInput().query(query).variables(variables).build();
 
     ExecutionResult executionResult = GQL.execute(executionInput);
