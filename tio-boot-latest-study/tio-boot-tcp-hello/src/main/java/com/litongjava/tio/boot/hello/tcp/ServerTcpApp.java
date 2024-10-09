61c3018542b98f6c35ca28d734b6c6fbdcb7d1b8
litongjava <litongjava@qq.com>
2024-10-09 12:11:16 -1000

    update aop version

--- tio-boot-latest-study/tio-boot-tcp-hello/src/main/java/com/litongjava/tio/boot/hello/tcp/ServerTcpApp.java
+++ tio-boot-latest-study/tio-boot-tcp-hello/src/main/java/com/litongjava/tio/boot/hello/tcp/ServerTcpApp.java
@@ -4,11 +4,11 @@ import com.litongjava.annotation.AComponentScan;
 import com.litongjava.tio.boot.TioApplication;
 
 @AComponentScan
-public class App {
+public class ServerTcpApp {
 
   public static void main(String[] args) {
     long start = System.currentTimeMillis();
-    TioApplication.run(App.class, args);
+    TioApplication.run(ServerTcpApp.class, args);
     long end = System.currentTimeMillis();
     System.out.println(end - start + "(ms)");
   }
